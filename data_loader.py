b69d224ec1e464ca1a50ddc52b0ccac896cecac1
philipp.merz <philipp.merz@bearingpoint.com>
2019-10-20 18:25:17 +0200

    output refined from dataloader, validation for deep net added

--- data_loader.py
+++ data_loader.py
@@ -38,6 +38,27 @@ def read_stocks_txt_to_df(stock_list):
 #                                   columns = [stock + '_close'])
     return close
 
+def stocks_vs_benchmark(stocks, date_t0, date_t1, date_t2):
+    
+    stocks_return = stocks.loc[date_t0:date_t2].dropna(1).filter(regex='(.+close)', 
+                                          axis=1).pct_change()
+    stocks_performance = (stocks_return +1).cumprod() 
+    
+    benchmark = (sp500_close.loc[date_t0:date_t2].pct_change()+1).cumprod()
+    stocks_performance = (stocks_return +1).cumprod()
+    
+    vs_benchmark_date = benchmark.index[-1]
+    stocks_vs_bench = stocks_performance.loc[\
+            vs_benchmark_date, :] > benchmark.loc[vs_benchmark_date][0]
+    out_path = dirpath+'\\returns_with_benchmark\\'+date_t0+'_'+date_t1
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+    stocks_vs_bench.to_csv(out_path+'\\VSbench.csv')
+    
+    stocks_performance.loc[date_t0:date_t1].to_csv(out_path+'\\performance.csv')
+    stocks_return.loc[date_t0:date_t1].to_csv(out_path+'\\returns.csv')
+    return vs_benchmark_date, out_path
+
 with open('sp500-historical-components_no_weighting.json') as data:
     data = json.load(data)
     time_stamps = []
@@ -54,10 +75,6 @@ stocks_2017_04_28 = read_stocks_txt_to_df(sp500_comp_df.loc['2017-04-28'].dropna
 
 sp500_close = pd.read_csv(dirpath + '\\spx.csv', sep=',', index_col ='date', parse_dates=True)
 
-# discrete or percentile change, why not log change?
-#aapl['Return'] = aapl['aapl_Close'].pct_change()
-# pl.hist(aapl['Return'].dropna(), bins = 75)
-#aapl_stats = ( np.mean(aapl['Return']), np.std(aapl.Return) )
 
 sp500_2004_leading_stocks = ["AAPL","XOM","GAS",'BVLAL','EEKEKW','SKKWKW'] # tote stocks haben keine auswirkung
 #['acor', 'chscp', 'fslr']    waren die schrott ?? ,"GAS","MIL","MXA"
@@ -71,31 +88,60 @@ sp500_2004_df = read_stocks_txt_to_df(sp500_2004_leading_stocks
 stocks_2017_04_28_portfolio = stocks_2017_04_28.filter(regex='.+close', 
                                       axis=1).pct_change()
 
-start_date = '2011-01-31'
-end_date = '2012-01-30'
+##### data polishing for deep learning
+## 2011-01-31
+#
+#stocks_2011_01_31 = read_stocks_txt_to_df(sp500_comp_df.loc['2011/01/31'].dropna(1).values[0])
+#stocks_2011_01_31_return = stocks_2011_01_31.loc['2011-01-31':'2013-01-30'].dropna(1).filter(regex='(.+close)', 
+#                                      axis=1).pct_change()
+#stocks_2011_01_31_performance = (stocks_2011_01_31_return +1).cumprod() 
+#
+#benchmark = (sp500_close.loc['2011-01-31':'2013-01-30'].pct_change()+1).cumprod()
+#stocks_2011_01_31_performance = (stocks_2011_01_31_return +1).cumprod()
+#
+#vs_benchmark_date = benchmark.index[-1]
+#stocks_vs_bench = stocks_2011_01_31_performance.loc[\
+#        vs_benchmark_date, :] > benchmark.loc[vs_benchmark_date][0]
+#stocks_vs_bench.to_csv('returns_with_benchmark\\VSbench.csv')
+#
+#(stocks_2011_01_31.loc['2011-01-31':'2012-01-31'].dropna(1).filter(regex='(.+close)', 
+#      axis=1).pct_change() +1).cumprod().to_csv('returns_with_benchmark\\returns.csv')
 
-stocks_2011_01_31 = read_stocks_txt_to_df(sp500_comp_df.loc['2011/01/31'].dropna(1).values[0])
-stocks_2011_01_31_return = stocks_2011_01_31.loc['2011-01-31':'2013-01-30'].dropna(1).filter(regex='(.+close)', 
-                                      axis=1).pct_change()
-stocks_2011_01_31_performance = (stocks_2011_01_31_return +1).cumprod() 
+## '2015-06-30'
+used_1_t0 = '2015-06-30'
+used_1_t1 = '2016-06-29'
+used_1_t2 = '2017-06-29'
 
-benchmark = (sp500_close.loc['2011-01-31':'2013-01-30'].pct_change()+1).cumprod()
-stocks_2011_01_31_performance = (stocks_2011_01_31_return +1).cumprod()
+stocks_df_1 = read_stocks_txt_to_df(sp500_comp_df.loc[used_1_t0].dropna(1).values[0])
 
-vs_benchmark_date = benchmark.index[-1]
-stocks_vs_bench = stocks_2011_01_31_performance.loc[\
-        vs_benchmark_date, :] > benchmark.loc[vs_benchmark_date][0]
-stocks_vs_bench.to_csv('returns_with_benchmark\\VSbench.csv')
+dummy = stocks_vs_benchmark(stocks_df_1, used_1_t0, used_1_t1, used_1_t2)
+#####
 
-(stocks_2011_01_31.loc['2011-01-31':'2012-01-31'].dropna(1).filter(regex='(.+close)', 
-      axis=1).pct_change() +1).cumprod().to_csv('returns_with_benchmark\\returns.csv')
+## '2014-06-30'
+used_2_t0 = '2014-06-30'
+used_2_t1 = '2015-06-30'
+used_2_t2 = '2016-06-30'
+
+stocks_df_2 = read_stocks_txt_to_df(sp500_comp_df.loc[used_2_t0].dropna(1).values[0])
+
+dummy = stocks_vs_benchmark(stocks_df_2, used_2_t0, used_2_t1, used_2_t2)
+
+## '2011-01-31'
+used_3_t0 = '2011-01-31'
+used_3_t1 = '2012-01-31'
+used_3_t2 = '2013-01-31'
+
+stocks_df_3 = read_stocks_txt_to_df(sp500_comp_df.loc[used_3_t0].dropna(1).values[0])
+
+dummy = stocks_vs_benchmark(stocks_df_3, used_3_t0, used_3_t1, used_3_t2)
+
+#####
 
 apple_vs_tote = read_stocks_txt_to_df(sp500_2004_leading_stocks)
 sp500_2004_df[0] = 1                                      
 # sp500_2004_df[0] = 1
-sp_performance = sp500_close.pct_change().loc['2006':'2017']
-sp_performance = sp_performance + 1
-sp_performance = sp_performance.cumprod()
+sp_performance = sp500_close.loc['1996':'2016'].pct_change()
+sp_performance = (sp_performance + 1 ).cumprod()
 portfolio_performance = sp500_2004_df.loc['2006':'2017'].cumprod()
 # cummultiplication
 
--- deep_net_beating_market.py
+++ deep_net_beating_market.py
@@ -1,47 +1,65 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.optimizers import adam
 import pandas as pd
 import os
 
 dirpath = os.getcwd()
+in_path = dirpath+ '\\returns_with_benchmark\\'
 
-dataset_df = pd.read_csv(dirpath+ '\\returns_with_benchmark\\returns.csv',
+def get_data(input_directory):
+    dataset_df = pd.read_csv(input_directory+'\\performance.csv',
                               sep=',', 
                               index_col='Date',
                               parse_dates = True)
-dataset = dataset_df.values[1:, :].T
-
-target_ser = pd.read_csv(dirpath+ '\\returns_with_benchmark\\VSbench.csv',
+    
+    target_ser = pd.read_csv(input_directory+'\\VSbench.csv',
                               sep=',',
                               index_col=0,
                               header= None,
                               names = ['VSbench'])
-target = np.array([int(i[0]) for i in target_ser.values])
-predicted_stocks = target_ser.index
+    return dataset_df, target_ser
+
+dataset_1, target_1 = get_data(in_path + '2011-01-31_2012-01-31')
+dataset_2, target_2 = get_data(in_path + '2014-06-30_2015-06-30')
+dataset_3, target_3 = get_data(in_path + '2015-06-30_2016-06-29')
+
+dataset = np.concatenate((dataset_1.values[1:, :].T, dataset_2.values[1:, :].T),
+                         axis = 0)
+target_ser = np.concatenate((target_1, target_2))
+
+vali_dataset = dataset_3.values[1:, :].T
+
+vali_target = np.array([int(i[0]) for i in target_3.values])
+target = np.array([int(i[0]) for i in target_ser])
+
 # split into input (dataset) and output (y) variables
 
 
 # define the keras model
 model = Sequential()
 model.add(Dense(100, input_dim=252, activation='relu'))
-model.add(Dense(60, activation='relu'))
+model.add(Dense(80, activation='relu'))
 model.add(Dense(40, activation='relu'))
 model.add(Dense(20, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 # compile the keras model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['binary_accuracy'])
+adam_opt = adam(lr = 0.01)
+model.compile(loss='binary_crossentropy', optimizer= adam_opt, metrics=['accuracy'])
 
 # fit the keras model on the dataset
-model.fit(dataset, target, epochs=150, batch_size=10)
+hist = model.fit(dataset, target, epochs=300, batch_size=10, verbose = 2, )
 
 # evaluate the keras model
-_, accuracy = model.evaluate(dataset, target)
+_, accuracy = model.evaluate(vali_dataset, vali_target)
 print('Accuracy: %.2f' % (accuracy*100))
 
 # make class predictions with the model
-predictions = model.predict_classes(dataset)
-# summarize the first 5 cases
-for i in range(20):
+# gives around 70.33% accuracy when trained with performance 
+# gives around 60 % accuracy when trained with return
+predictions = model.predict_classes(vali_dataset)
+# summarize the first few cases
+for i in range(10):
 	print('%s => %d (expected %d)' % (dataset[i][:5], predictions[i], target[i]))
